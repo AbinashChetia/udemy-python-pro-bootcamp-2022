d7bc337faf303754e55d364d261d3926fa5dfc9f
AbinashChetia <abinashdu2000@gmail.com>
2022-02-08 19:53:05 +0530

    Update 3_treasureIsland.py

--- 3_treasureIsland.py
+++ 3_treasureIsland.py
@@ -26,10 +26,10 @@ Your mission is to find the treasure.
 prize = '''
 You opened the door and you see a treasure chest. Congrats you found the treasure!\n
          __________
-        /\____;;___\\
+        /\____;;___\
        | /         /
        `. ())oo() .
-        |\(%()*^^()^\\
+        |\(%()*^^()^\
        %| |-%-------|
       % \ | %  ))   |
       %  \|%________|
@@ -40,7 +40,21 @@ if opt1 == 'left':
     opt2 = input('You arrived at a lake. There\'s an island in the middle of the lake. Type "wait" to wait for a boat or type "swim" to swim across.\n').lower()
     if opt2 == 'wait':
         opt3 = input('You get a boat and arrived at the island. There\'s a house with 3 doors: one red, one yellow and one blue. Type "red", "yellow" or "blue" to choose one of the doors.\n').lower()
-        if opt3 == 'blue':
+        if opt3 == 'yellow':
             print(prize)
-        elif opt3 == 'yellow':
-            print('You have ')
+        elif opt3 == 'blue':
+            print('You are eaten by Beasts.\nGAME OVER!!!\n')
+        elif opt3 == 'red':
+            print('You are burned by fire.\nGAME OVER!!!\n')
+        else:
+            print('The door you have selected doesn\'t exist.\nGAME OVER!!!\n')
+    elif opt2 == 'swim':
+        print('You are attacked by trout.\nGAME OVER!!!\n')
+    else:
+        print('You have entered an invalid choice.\nGAME OVER!!!\n')
+elif opt1 == 'right':
+    print('You fall into a hole.\nGAME OVER!!!\n')
+else:
+    print('You have entered an invalid choice.\nGAME OVER!!!\n')
+
+        
