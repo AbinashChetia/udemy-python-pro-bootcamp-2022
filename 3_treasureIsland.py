b344832115c1b713e3867bd6d8ffe12a35b01a59
AbinashChetia <abinashdu2000@gmail.com>
2022-02-08 19:31:59 +0530

    Update 3_treasureIsland.py

--- 3_treasureIsland.py
+++ 3_treasureIsland.py
@@ -23,5 +23,24 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 Welcome to Treasure Island.
 Your mission is to find the treasure.
 '''
+prize = '''
+You opened the door and you see a treasure chest. Congrats you found the treasure!\n
+         __________
+        /\____;;___\\
+       | /         /
+       `. ())oo() .
+        |\(%()*^^()^\\
+       %| |-%-------|
+      % \ | %  ))   |
+      %  \|%________|
+'''
 print(welcome)
-opt1 = input('You are at a cross road. Where do you want to go? Type "left" or "right"')
+opt1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"\n').lower()
+if opt1 == 'left':
+    opt2 = input('You arrived at a lake. There\'s an island in the middle of the lake. Type "wait" to wait for a boat or type "swim" to swim across.\n').lower()
+    if opt2 == 'wait':
+        opt3 = input('You get a boat and arrived at the island. There\'s a house with 3 doors: one red, one yellow and one blue. Type "red", "yellow" or "blue" to choose one of the doors.\n').lower()
+        if opt3 == 'blue':
+            print(prize)
+        elif opt3 == 'yellow':
+            print('You have ')
